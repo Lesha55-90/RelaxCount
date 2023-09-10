459b6d6966c51a946d54cb7a82284979ea799963
Aleksey <Lesha55-90@mail.ru>
2023-09-10 20:40:15 +0600

    initial commit

---
+++ src/test/java/ru/netology/services/RelaxServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RelaxServiceTest {
+    @Test
+    public void shouldCalcExact() {
+        RelaxService service = new RelaxService();
+
+        int expected = 2;
+        int actual = service.calcRelax(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
